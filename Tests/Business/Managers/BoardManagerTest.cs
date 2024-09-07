81b8088e6ab08250b091107b832ea0bb2aa7362b
Rodrigo Verçosa <rodrigo.vercosa@crunchyroll.com>
2024-09-07 08:51:28 -0300

    fixing tests

---
+++ Tests/Business/Managers/BoardManagerTest.cs
@@ -0,0 +1,79 @@
+﻿using ConwayGameOfLife.Business.Managers;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwayGameOfLife.Tests.Business.Managers
+{
+    public class BoardManagerTest
+    {
+        public class TheCalculateNextGridStateMethod : BoardManagerTest
+        {
+            [Fact]
+            public void ShouldCorrectlyCalculateNextStateForLiveAndDeadCells()
+            {
+                string initialState = @"[[1,0,0],[0,1,0],[0,0,1]]";
+                var board = new Board(initialState);
+                var boardManager = new BoardManager();
+
+                boardManager.CalculateNextGridState(board);
+
+                var expectedState = @"[[0,0,0],[0,1,0],[0,0,0]]";
+                Assert.Equal(board.State, expectedState);
+            }
+        }
+
+        public class TheCalculateStateXStepsAwayMethod : BoardManagerTest
+        {
+            [Fact]
+            public void ShouldCorrectlyCalculateStateAfterXSteps()
+            {
+                string initialState = @"[[0,1,0],[0,0,1],[1,1,1]]";
+                var board = new Board(initialState);
+
+                var boardManager = new BoardManager();
+                int steps = 2;
+
+                boardManager.CalculateStateXStepsAway(board, steps);
+
+                var expectedState = @"[[0,0,0],[0,0,1],[0,1,1]]";
+
+                Assert.Equal(board.State, expectedState);
+            }
+        }
+
+        public class TheCalculateFinalStateMethod : BoardManagerTest
+        {
+            [Fact]
+            public void ShouldStabilizeAndReturnFinalStateBeforeMaxSteps()
+            {
+                string initialState = @"[[1,1,0],[1,1,0],[0,0,0]]"; //This config keeps the board stable
+                var board = new Board(initialState);
+
+                var boardManager = new BoardManager();
+                int maxSteps = 10;
+
+                boardManager.CalculateFinalState(board, maxSteps);
+
+                var expectedState = @"[[1,1,0],[1,1,0],[0,0,0]]";
+                Assert.Equal(board.State, expectedState);
+            }
+
+            [Fact]
+            public void ShouldThrowExceptionIfBoardDoesNotStabilizeAfterMaxSteps()
+            {
+                var initialState = @"[[0,1,0],[0,1,0],[0,1,0]]"; //This config generates an oscilator
+                var board = new Board(initialState);
+
+                var boardManager = new BoardManager();
+                int maxSteps = 5;
+
+                var exception = Assert.Throws<InvalidOperationException>(() => boardManager.CalculateFinalState(board, maxSteps));
+                Assert.Equal($"Board did not stabilize after {maxSteps} steps.", exception.Message);
+            }
+        }
+    }
+}
---
+++ Tests/Business/Services/BoardServiceTest.cs
@@ -0,0 +1,151 @@
+using ConwayGameOfLife.Business.Services;
+using ConwayGameOfLife.Business.Services.Interfaces;
+using ConwayGameOfLife.Infra.Data.Interfaces;
+using Domain.Entities;
+using Moq;
+
+namespace ConwayGameOfLife.Tests.Business.Services
+{
+    public class BoardServiceTest
+    {
+        private string _serializedInitialState = @"[
+                [0, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 0, 0, 1, 1, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 1, 1, 1, 0, 0, 0, 1, 0],
+                [0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 1, 1, 0, 1, 0, 1],
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 1, 0, 0, 0, 1, 1, 1, 1, 0]
+                ]";
+
+        public class TheAddBoardMethod : BoardServiceTest
+        {
+            [Fact]
+            public async Task ShouldReturnBoardIdAfterAddingBoard()
+            {
+                var expectedBoardId = 1;
+                var boardRepositoryMock = new Mock<IBoardRepository>();
+                boardRepositoryMock
+                    .Setup(repo => repo.AddBoardAsync(It.IsAny<Board>()))
+                    .ReturnsAsync(expectedBoardId);
+                var boardManagerMock = new Mock<IBoardManager>();
+
+                var boardService = new BoardService(boardManagerMock.Object, boardRepositoryMock.Object);
+                var result = await boardService.AddBoardAsync(_serializedInitialState);
+
+                Assert.Equal(expectedBoardId, result);
+            }
+        }
+
+        public class TheGetNextStateMethod : BoardServiceTest
+        {
+            [Fact]
+            public async Task ShouldReturnNextStateGrid()
+            {
+                var boardId = 1;
+                var board = new Board(_serializedInitialState);
+                var expectedGrid = new int[3, 3];
+
+                var boardRepositoryMock = new Mock<IBoardRepository>();
+                boardRepositoryMock.Setup(repo => repo.GetBoardByIdAsync(boardId)).ReturnsAsync(board);
+                var boardManagerMock = new Mock<IBoardManager>();
+                boardManagerMock.Setup(manager => manager.CalculateNextGridState(board)).Callback(() =>
+                {
+                    string serializedNextState = @"[
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                            ]";
+                    board.SetupBoard(serializedNextState);
+                });
+
+                var boardService = new BoardService(boardManagerMock.Object, boardRepositoryMock.Object);
+                var result = await boardService.CalculateNextStateAsync(boardId);
+
+                Assert.Equal(board.State, result);
+            }
+        }
+
+        public class TheGetStateXStepsAwayMethod : BoardServiceTest
+        {
+            [Fact]
+            public async Task ShouldReturnGridStateAfterXSteps()
+            {
+                var boardId = 1;
+                var xSteps = 5;
+                var board = new Board(_serializedInitialState);
+
+                var boardRepositoryMock = new Mock<IBoardRepository>();
+                boardRepositoryMock.Setup(repo => repo.GetBoardByIdAsync(boardId)).ReturnsAsync(board);
+                var boardManagerMock = new Mock<IBoardManager>();
+                boardManagerMock.Setup(manager => manager.CalculateStateXStepsAway(board, xSteps)).Callback(() =>
+                {
+                    string serializedNextState = @"[
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+                            ]";
+                    board.SetupBoard(serializedNextState);
+                });
+
+                var boardService = new BoardService(boardManagerMock.Object, boardRepositoryMock.Object);
+                var result = await boardService.GetStateXStepsAwayAsync(boardId, xSteps);
+
+                Assert.Equal(board.State, result);
+            }
+        }
+
+        public class TheGetFinalStateMethod : BoardServiceTest
+        {
+            [Fact]
+            public async Task ShouldReturnFinalStateGrid()
+            {
+                var boardId = 1;
+                var xSteps = 5;
+                var board = new Board(_serializedInitialState);
+
+                var boardRepositoryMock = new Mock<IBoardRepository>();
+                boardRepositoryMock.Setup(repo => repo.GetBoardByIdAsync(boardId)).ReturnsAsync(board);
+                var boardManagerMock = new Mock<IBoardManager>();
+                boardManagerMock.Setup(manager => manager.CalculateFinalState(board, xSteps)).Callback(() =>
+                {
+                    string serializedNextState = @"[
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+                            ]";
+                    board.SetupBoard(serializedNextState);
+                });
+
+                var boardService = new BoardService(boardManagerMock.Object, boardRepositoryMock.Object);
+                var result = await boardService.GetFinalStateAsync(boardId, boardId);
+
+                Assert.Equal(board.State, result);
+            }
+        }
+    }
+}
